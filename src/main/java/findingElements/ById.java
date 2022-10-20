64682a950e67580f286427c1a954d29b66d102cb
Krzysztof Bednarczyk <krisu876@gmail.com>
2022-10-20 16:50:24 +0200

    Search ById script

---
+++ src/main/java/findingElements/ById.java
@@ -0,0 +1,26 @@
+package findingElements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ById {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver",
+                "src/main/resources/drivers/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://hotel-testlab.coderslab.pl/en/");
+        WebElement hotelLocationInput = driver.findElement(By.id("hotel_location"));
+        WebElement searchNowButton = driver.findElement(By.id("search_room_submit")); // not used
+        WebElement enterEmailInput = driver.findElement(By.id("newsletter-input"));
+
+        hotelLocationInput.sendKeys("Warsaw");
+        enterEmailInput.sendKeys("test@test.com");
+
+        driver.quit();
+    }
+}
