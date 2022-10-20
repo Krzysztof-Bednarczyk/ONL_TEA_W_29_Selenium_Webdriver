6ef34bf811151275e7317daabe5831b434b765cc
Krzysztof Bednarczyk <krisu876@gmail.com>
2022-10-20 15:29:21 +0200

    Added BrowseWebsites script

--- src/main/java/TaskCheckConfig/BrowseWebsites.java
+++ src/main/java/TaskCheckConfig/BrowseWebsites.java
@@ -1,2 +1,19 @@
-package TaskCheckConfig;public class BrowseWebsites {
+package TaskCheckConfig;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowseWebsites {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver",
+                "src/main/resources/drivers/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://coderslab.pl/pl");
+        driver.get("https://mystore-testlab.coderslab.pl/index.php");
+        driver.get("https://hotel-testlab.coderslab.pl/en/");
+        driver.quit();
+    }
 }
