7d6f09681df26bc459e5d661eb0f6542a0819f2b
Krzysztof Bednarczyk <krisu876@gmail.com>
2022-10-20 15:36:41 +0200

    Added Navigation script

--- src/main/java/TaskCheckConfig/Navigation.java
+++ src/main/java/TaskCheckConfig/Navigation.java
@@ -1,2 +1,23 @@
-package TaskCheckConfig;public class Navigation {
+package TaskCheckConfig;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Navigation {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver",
+                "src/main/resources/drivers/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+        driver.get("https://coderslab.pl/pl");
+        driver.navigate().back();
+        driver.get("https://mystore-testlab.coderslab.pl/index.php");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+        driver.quit();
+    }
 }
