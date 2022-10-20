37e865668d45e258e13205671972e093329a45cc
Krzysztof Bednarczyk <krisu876@gmail.com>
2022-10-20 17:03:29 +0200

    Search ByClassname script

---
+++ src/main/java/findingElements/ByClassname.java
@@ -0,0 +1,28 @@
+package findingElements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ByClassname {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver",
+                "src/main/resources/drivers/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://hotel-testlab.coderslab.pl/en/");
+        WebElement signInButton = driver.findElement(By.className("user_login"));
+        signInButton.click();
+        WebElement emailInput = driver.findElement(By.className("account_input"));
+        emailInput.sendKeys("test@test.com");
+        WebElement createAccountButton = driver.findElement(By.name("SubmitCreate"));
+        createAccountButton.submit();
+
+        driver.quit();
+
+    }
+}
