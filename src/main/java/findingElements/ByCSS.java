b354251e41fabae59c0d65e284c484aa8f2856f4
Krzysztof Bednarczyk <krisu876@gmail.com>
2022-10-21 08:33:28 +0200

    Added ByCSS script

---
+++ src/main/java/findingElements/ByCSS.java
@@ -0,0 +1,52 @@
+package findingElements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static utils.DataFaker.*;
+
+public class ByCSS {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver",
+                "src/main/resources/drivers/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS) ;
+        driver.manage().window().maximize();
+        driver.get("https://hotel-testlab.coderslab.pl/en/");
+        WebElement signInButton = driver.findElement(By.className("user_login"));
+        signInButton.click();
+        WebElement emailInput = driver.findElement(By.className("account_input"));
+        emailInput.sendKeys(createRandomEmail());
+        WebElement createAccountButton = driver.findElement(By.name("SubmitCreate"));
+        createAccountButton.submit();
+
+
+        // Adding Xpath code
+
+        WebElement firstNameInput = driver.findElement(By.xpath("//input[@id='customer_firstname']"));
+        WebElement lastNameInput = driver.findElement(By.xpath("//input[@id='customer_lastname']"));
+        WebElement emailFormInput = driver.findElement(By.xpath("//input[@id='email']"));
+        WebElement password = driver.findElement(By.xpath("//input[@id='passwd']"));
+
+        firstNameInput.sendKeys(createRandomFirstName());
+        lastNameInput.sendKeys(createRandomLastName());
+        emailFormInput.clear();
+        emailFormInput.sendKeys(createRandomEmail());
+        password.sendKeys(createRandomPassword());
+
+        WebElement registerButton = driver.findElement(By.xpath("//button[@id='submitAccount']"));
+        registerButton.click();
+
+        // Adding CSS code
+        WebElement myPersonalInfo = driver.findElement(By.cssSelector("a[title='Information']"));
+        WebElement myAdresses = driver.findElement(By.cssSelector("a[title='Addresses']"));
+
+        driver.quit();
+    }
+}
