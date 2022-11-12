2670b200b903e027c36897610813f0ba9caa7d60
Krzysztof Bednarczyk <krisu876@gmail.com>
2022-11-12 10:52:25 +0100

    First JUnit test

---
+++ src/test/java/coderslab/examples/MyFirstJUnitTest.java
@@ -0,0 +1,58 @@
+package coderslab.examples;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+import static utils.DataFaker.*;
+
+class MyFirstJUnitTest {
+
+    @Test
+    void registerUserTest(){
+        System.setProperty("webdriver.chrome.driver",
+                "src/main/resources/drivers/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://hotel-testlab.coderslab.pl/en/");
+        WebElement signInButton = driver.findElement(By.className("user_login"));
+        signInButton.click();
+        WebElement emailInput = driver.findElement(By.className("account_input"));
+        emailInput.sendKeys(createRandomEmail());
+        WebElement createAccountButton = driver.findElement(By.name("SubmitCreate"));
+        createAccountButton.submit();
+
+
+        // Adding Xpath code
+
+        WebElement firstNameInput = driver.findElement(By.xpath("//input[@id='customer_firstname']"));
+        WebElement lastNameInput = driver.findElement(By.xpath("//input[@id='customer_lastname']"));
+        WebElement emailFormInput = driver.findElement(By.xpath("//input[@id='email']"));
+        WebElement password = driver.findElement(By.xpath("//input[@id='passwd']"));
+
+        firstNameInput.sendKeys(createRandomFirstName());
+        lastNameInput.sendKeys(createRandomLastName());
+        emailFormInput.clear();
+        emailFormInput.sendKeys(createRandomEmail());
+        password.sendKeys(createRandomPassword());
+
+        WebElement registerButton = driver.findElement(By.xpath("//button[@id='submitAccount']"));
+        registerButton.click();
+
+        // Adding CSS code
+        WebElement myPersonalInfo = driver.findElement(By.cssSelector("a[title='Information']"));
+        WebElement myAdresses = driver.findElement(By.cssSelector("a[title='Addresses']"));
+
+        Assertions.assertTrue(myPersonalInfo.isDisplayed());
+        Assertions.assertTrue(myAdresses.isDisplayed());
+
+        driver.quit();
+    }
+}
